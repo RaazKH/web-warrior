2152f1cfb2e98327ac46b79b5edc96db5ff533ef
RaazKH <serxtdweia9r@gmail.com>
2021-04-18 16:01:28 +0300

    Added a bunch of plugins for image optimization

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,8 @@ module.exports = {
   /* Your site config here */
   plugins: [
     'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -15,6 +17,13 @@ module.exports = {
         path: `${__dirname}/src/projects/`
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`
+      },
+    }
   ],
   siteMetadata: {
     title: 'Web Warrior',
