6ccb18bbc8947915fbf776cd8c413f85cf23238c
RaazKH <serxtdweia9r@gmail.com>
2021-04-18 08:59:40 +0300

    Created a layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function About() {
     return (
         <div>
-            <h1>About Page</h1>
-            <p>Some about page text.</p>
+            <Layout>
+                <h1>About Page</h1>
+                <p>Some about page text.</p>
+            </Layout>
         </div>
     )
 }
