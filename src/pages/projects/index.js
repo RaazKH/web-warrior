682a2eaa87c9719b052fa4b4ed5ac71065aad1c6
RaazKH <serxtdweia9r@gmail.com>
2021-04-18 09:58:07 +0300

    Added global and module styles

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import * as styles from '../../styles/projects.module.css'
 
 export default function Projects() {
     return (
-        <div>
+        <div className={styles.portfolio}>
             <Layout>
                 <h2>Portfolio</h2> 
                 <h3>Projects & websites I've Created</h3>
