ad51a1053ab902dbbab320526d16f9607fef7964
RaazKH <serxtdweia9r@gmail.com>
2021-04-18 15:05:50 +0300

    Pulling data from markdown files

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,44 @@
+import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from '../../styles/projects.module.css'
 
-export default function Projects() {
+export default function Projects({ data }) {
+    console.log(data)
+    const projects = data.allMarkdownRemark.nodes
+
     return (
         <div className={styles.portfolio}>
             <Layout>
                 <h2>Portfolio</h2> 
                 <h3>Projects & websites I've Created</h3>
+                <div className={styles.projects}>
+                    {projects.map(project => (
+                        <Link to={'/projects/' + project.frontmatter.slug} key={project.id}>
+                            <div>
+                                <h3>{ project.frontmatter.title }</h3>
+                                <p>{ project.frontmatter.stack }</p>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
             </Layout>
         </div>
     )
 }
+
+// export page query
+export const query = graphql`
+query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+}
+`
